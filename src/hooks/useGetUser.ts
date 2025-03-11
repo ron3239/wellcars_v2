b623a8ee87381be8f2e866a6ccd81d743999a5fc
ron <jook323@gmail.com>
2025-03-11 19:33:41 +0300

    qwe

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -1,7 +1,7 @@
 import { DataBase } from '@/services/DataBase'
 import { useQuery } from '@tanstack/react-query'
 export function useGetUser(id_user: string) {
-  const id_user_int = parseInt(id_user)
+  // const id_user_int = parseInt(id_user)
   const { data, isLoading, isError } = useQuery({
     queryKey: ['user'],
     queryFn: () => DataBase.GetUser(id_user),
